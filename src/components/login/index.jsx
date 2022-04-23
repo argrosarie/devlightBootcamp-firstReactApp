99aab7ca6d66f83a0b1f61f6cb8c73733a534be1
rosarie <rosariosanchezsampietro@gmail.com>
2022-04-23 11:21:38 -0300

    First commit: I modified my create-react-app template and started my project with main structure of login

---
+++ src/components/login/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './login.css';
+
+const Login = () => {
+  return (
+   <div className="login">
+       <h1 className="login-title">
+           Stax Food Admin Login
+       </h1>
+       <div className="login-modal">
+           <p className="login-modal_directions">Please fill in your unique admin login details below</p>
+           
+       </div>
+   </div>
+  )
+}
+
+export default Login;
